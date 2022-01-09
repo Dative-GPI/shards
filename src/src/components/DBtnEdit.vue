382dfd038a02febdfe32b6e731f389d40810c485
Quentin SCHROTER <qschroter@dative-gpi.com>
2022-01-09 16:04:25 +0100

    remove old logic

--- src/src/components/DBtnEdit.vue
+++ src/src/components/DBtnEdit.vue
@@ -2,8 +2,6 @@
   <d-btn
     v-bind="$attrs"
     class="d-btn-edit blue-4"
-    :class="{ active: value }"
-    @click="$emit('input', !value)"
     :icon="$attrs.icon || 'mdi-pencil'"
   >
     <span class="grey-3--text">
@@ -19,7 +17,5 @@ import { Component, Prop, Vue } from "vue-property-decorator";
   inheritAttrs: false,
 })
 export default class DBtnEdit extends Vue {
-  @Prop({ required: false, default: false })
-  value!: boolean;
 }
 </script>
