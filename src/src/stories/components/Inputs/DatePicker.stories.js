a3961fbbc61166673f0a8bfa56959500d8e96eb4
qschroter <qschroter@gpi-experts.com>
2022-11-21 18:08:07 +0100

    [skip ci] add stories for datepicker but cannot reproduce error

--- src/src/stories/components/Inputs/DatePicker.stories.js
+++ src/src/stories/components/Inputs/DatePicker.stories.js
@@ -1,3 +1,4 @@
+import { parseISO } from "date-fns";
 import DDatePicker from '@/components/DDatePicker.vue';
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
@@ -31,5 +32,18 @@ const Template = (args, { argTypes }) => ({
 export const Default = Template.bind({});
 Default.args = {
   value: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 23, 0, 0),
+  contentClass: "my-5"
+};
+
+export const ErrorTooLow = Template.bind({});
+Default.args = {
+  value: parseISO("0001-01-01T00:00:00"),
+  contentClass: "my-5"
+};
+
+
+export const ErrorTooHigh = Template.bind({});
+Default.args = {
+  value: parseISO("9999-12-31T23:59:59.999999"),
   contentClass: "my-5"
 };
