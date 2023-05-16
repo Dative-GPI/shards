7b96e29ffa40da93125b7cecafc94ee9602f34d0
rduteil <romain.duteil.insa@gmail.com>
2023-05-16 15:53:17 +0200

    [0.1.73] Drawer with scrollbars on hover

--- src/src/components/DRightDrawer.vue
+++ src/src/components/DRightDrawer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-navigation-drawer app right temporary v-on="$listeners" v-bind="$attrs" class="pa-2">
     <slot name="header">
-      <div class="d-drawer-header align-start justify-start">
+      <div class="d-drawer-header align-start justify-start mb-5">
         <d-btn icon @click="$emit('input', false)">
           <v-icon large>
             mdi-chevron-right
@@ -17,13 +17,9 @@
       </div>
     </slot>
 
-    <div class="d-flex mt-10">
+    <div class="d-scrollbarmock" style="max-height: calc(100% - 50px) !important;">
       <slot></slot>
     </div>
-
-    <v-footer absolute v-if="$scopedSlots['actions']" color="white-1">
-      <slot name="actions"></slot>
-    </v-footer>
   </v-navigation-drawer>
 </template>
 
@@ -38,22 +34,11 @@ export default class DRightDrawer extends Vue {
 </script>
 
 <style scoped>
-::v-deep .v-navigation-drawer__content {
-  overflow-y: scroll;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-}
-
-::v-deep .v-navigation-drawer__content::-webkit-scrollbar {
-  display: none;
-}
-
 ::v-deep .d-drawer-header {
-  z-index: 5;
-  width: 100%;
+  position: sticky;
   display: flex;
-  flex-wrap: nowrap;
-  position: absolute;
+  height: 30px;
+  top: 0;
   background-color: var(--v-white-1-base);
 }
 </style>
